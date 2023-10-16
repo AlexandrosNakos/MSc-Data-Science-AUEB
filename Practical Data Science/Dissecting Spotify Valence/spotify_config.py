8f86304f8cd2247e5aeac729d63cc13ea882947e
AlexandrosNakos <103275413+AlexandrosNakos@users.noreply.github.com>
2023-10-16 16:01:07 +0300

    Add files via upload

---
+++ Practical Data Science/Dissecting Spotify Valence/spotify_config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 19:43:22 2021
+
+@author: anakos
+"""
+
+config = {
+    'client_id' : 'xxxxxxxxxxxxxxxxxxxxx',
+    'client_secret' :'xxxxxxxxxxxxxxxxxxx'
+}
